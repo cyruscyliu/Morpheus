84da54456dc8767853f16b003beb0d03e4cb5caf
Qiang Liu <cyruscyliu@gmail.com>
2026-04-29 10:16:10 +0200

    docs: publish next export

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
-
